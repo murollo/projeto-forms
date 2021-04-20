f0dfe8cc6a9ba89911723e37bc2cfaffb74e6ca0
murollo <murillo4493@gmail.com>
2021-04-20 20:50:23 -0300

    terminado

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Pessoa } from '../pessoa';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent implements OnInit {
+export class FormularioComponent {
 
-  constructor() { }
+  pessoa: Pessoa = new Pessoa();
 
-  ngOnInit(): void {
-  }
+  profissoes = ["Engenheiro", "Professor", "Quimico", "Zoólogo"];
 
   salvar(pessoaForm: NgForm) {
+    this.pessoa.nome = pessoaForm.value.nome;
+    this.pessoa.fone = pessoaForm.value.fone;
+    this.pessoa.email = pessoaForm.value.email;
+    this.pessoa.profissao = pessoaForm.value.profissao;
+    console.log(this.pessoa);
     console.log(pessoaForm);
     }
 
